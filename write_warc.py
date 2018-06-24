f550bf2bf627ad0e38661cd7b131db0d62e27f6f
breakthoven <breakthoven@localhost.localdomain>
2018-06-24 18:42:50 +0200

    probamos a grabar el fichero con append en varias pasadas
    Signed-off-by: breakthoven <breakthoven@localhost.localdomain>

--- write_warc.py
+++ write_warc.py
@@ -25,4 +25,22 @@ if __name__ == '__main__':
         
 
     f.close()
+
+
+    for u in urls:
+        f = warc.open("test_trozos.warc.gz", "a")
+        fp = urllib.request.urlopen( u )
+
+        mybytes = fp.read()
+        mystr = mybytes.decode("utf8")
+        
+        fp.close()        
+
+        header = h = warc.WARCHeader({"WARC-Type": "response"}, defaults=True)
+        header['WARC-Target-URI:'] = u
+        
+        record = warc.WARCRecord(header, mybytes)
+        f.write_record( record )
+
+        f.close()
     
